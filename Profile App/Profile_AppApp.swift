98ef1bf1622a9035a89497b2e87c0abf3d3cf20c
sanket-forge <sanketkhatua9861@gmail.com>
2024-12-08 16:48:47 +0530

    Initial Commit

---
+++ Profile App/Profile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_AppApp.swift
+//  Profile App
+//
+//  Created by Sanket Khatua on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Profile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
